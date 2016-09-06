20aaa1d678cb0265cf8726b4e960a0e9beb117cc
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-09-06 22:52:46 +0300

    Modified reverse geolocation to use correct language-settings

--- src/services/StoreLocatorService.js
+++ src/services/StoreLocatorService.js
@@ -2,9 +2,25 @@ import NodeGeocoder from "node-geocoder";
 import request from 'request';
 import {processingRequest, storesLoaded, requestFailed} from '../actions/StoreActions'
 
+import HttpsAdapter from "node-geocoder/lib/httpadapter/httpsadapter.js";
+
 const K_CHAIN_API = "http://www.k-market.fi/api/stores/searchStores";
 
-const geocoder = NodeGeocoder();
+const httpAdapter = new HttpsAdapter(null, {
+  headers: {
+    "Accept-Language": "fi-FI",
+    "Content-Language": "fi-FI"
+  }
+});
+
+const geoOptions = {
+  provider: 'google',
+  language: "fi-FI",
+  region: "FI",
+  httpAdapter: httpAdapter
+}
+
+const geocoder = NodeGeocoder(geoOptions);
 
 export const getAdjacentStores = (location) => {
   let queryAddress = K_CHAIN_API + "?query=" + location;
@@ -21,13 +37,13 @@ export const getAdjacentStores = (location) => {
   }
 }
 
-const loadStoresForLocation = (location, dispatch) => {
-  request("http://localhost:3000/k-proxy/:" +location, (error, response, body) => {
+const loadStoresForLocation = (geoLocation, dispatch) => {
+  request("http://localhost:3000/k-proxy/:" +geoLocation.city, (error, response, body) => {
     if(error) {
       console.log(error);
       dispatch(requestFailed());
     } else {
-      dispatch(storesLoaded(JSON.parse(body), location));
+      dispatch(storesLoaded(JSON.parse(body), geoLocation.formattedAddress));
     }
   });
 }
@@ -51,8 +67,7 @@ const getGeolocation = (callback, dispatch) => {
 const reverseGeolocation = (geoLocation, dispatch) => {
   geocoder.reverse({lat: geoLocation.coords.latitude, lon: geoLocation.coords.longitude})
   .then(res => {
-    const cityName = res.raw.results[1].address_components[0].long_name;
-    loadStoresForLocation(cityName, dispatch);
+    loadStoresForLocation(res[0], dispatch);
   })
   .error(err => console.log(err));
 }
